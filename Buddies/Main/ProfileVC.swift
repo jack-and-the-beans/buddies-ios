1708018d993695306f89bbc4a06144b303bf3c32
Jake Thurman <jacob@thurmans.com>
2019-01-30 19:40:20 -0500

    Fix issue after rebasing

---
+++ Buddies/Main/ProfileVC.swift
@@ -0,0 +1,45 @@
+//
+//  ProfileVC.swift
+//  Buddies
+//
+//  Created by Jake Thurman on 1/30/19.
+//  Copyright © 2019 Jack and the Beans. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ProfileVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func showMessagePrompt(_ msg: String) {
+        print("hit")
+        let alertController = UIAlertController(title: "Login Error", message: msg, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func signOut(_ sender: Any) {
+        let auth = AuthHandler(auth: Auth.auth())
+        auth.signOut(onError: showMessagePrompt) {
+            BuddiesStoryboard.Login.goTo()
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
