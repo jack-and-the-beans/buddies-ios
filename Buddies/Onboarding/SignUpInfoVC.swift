7dff4976ec757f15393640fed184b6f782072289
Jake Thurman <jacob@thurmans.com>
2019-02-06 14:24:21 -0500

    Pull out settings screen & load profile

--- Buddies/Onboarding/SignUpInfoVC.swift
+++ Buddies/Onboarding/SignUpInfoVC.swift
@@ -243,24 +243,11 @@ class SignUpInfoVC: LoginBase, UIImagePickerControllerDelegate, UINavigationCont
             alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
             
             self.present(alert, animated: true)
-        }else
-        {
+        }
+        else {
             saveBioToFirestore(bio: bioText.text)
             fillDataModel()
             BuddiesStoryboard.Main.goTo()
         }
-        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Buddies/Profile/ProfileVC.swift
+++ Buddies/Profile/ProfileVC.swift
@@ -10,115 +10,90 @@ import UIKit
 import Firebase
 
 class ProfileVC: UIViewController {
+    @IBOutlet weak var profilePic: UIButton!
+    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        profilePic.layer.cornerRadius = profilePic.frame.size.width / 2
+
+        loadProfileData()
     }
     
-    @IBAction func deleteAccount(_ sender: Any) {
+    
+    func loadProfileData(auth: Auth = Auth.auth(),
+                        storageManger: StorageManager = StorageManager.shared,
+                        users: CollectionReference = Firestore.firestore().collection("users")) {
         
-        if usedFacebook(){
+        let uid = auth.currentUser!.uid
+        
+        if let image = storageManger.getSavedImage(filename: uid) {
+            onImageLoaded(image: image)
             
-            let alert = UIAlertController(title: "Delete your account?", message: "Please confirm if you'd like to delete your account.", preferredStyle: .alert)
-            
-            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: {action in
-                self.deleteAccountFacebook()
-                }))
-            alert.addAction(UIAlertAction(title: "No", style: .destructive, handler: nil))
-            
-            self.present(alert, animated: true)
-        }else
-        {
-            self.performSegue(withIdentifier: "confirmDeleteAccountEmail", sender: self)
+            loadFromFile(uid: uid, needsImage: false, storageManger: storageManger, doc: users.document(uid))
+        } else {
+            loadFromFile(uid: uid, needsImage: true, storageManger: storageManger, doc: users.document(uid))
         }
-        
-        
     }
     
+    func onImageLoaded(image: UIImage) {
+        profilePic.tintColor = UIColor.clear
+        profilePic.setImage(image, for: .normal)
+    }
     
-    func usedFacebook(
-        user: UserInfo? = Auth.auth().currentUser,
-        collection: CollectionReference = Firestore.firestore().collection("users")) -> Bool{
+    func loadFromFile(uid: String,
+                      needsImage: Bool,
+                      storageManger: StorageManager,
+                      doc: DocumentReference) {
         
-        var fbFlag = false
-        
-        if let providerData = Auth.auth().currentUser?.providerData {
-            for userInfo in providerData {
-                switch userInfo.providerID {
-                case "facebook.com":
-                    fbFlag = true
-                default:
-                    fbFlag = false
+        doc.getDocument { (snapshot, err) in
+            guard let snapshot = snapshot else {
+                print("Error loading profile document \(err!.localizedDescription)")
+                return
+            }
+            
+            let data = snapshot.data()
+            
+            if (needsImage) {
+                guard let firebaseImageURL = data?["image_url"] as? String else {
+                    print("Cannot get Image URL for \(snapshot.documentID)")
+                    return
                 }
-            }
-        }
-        
-        return fbFlag
-    
-    }
-    
-    func showMessagePrompt(_ msg: String) {
-        print("hit")
-        let alertController = UIAlertController(title: "Login Error", message: msg, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
-        
-        self.present(alertController, animated: true, completion: nil)
-    }
-    
-    @IBAction func signOut(_ sender: Any) {
-        let auth = AuthHandler(auth: Auth.auth())
-        auth.signOut(onError: showMessagePrompt) {
-            BuddiesStoryboard.Login.goTo()
-        }
-    }
-    
-    
-    func deleteAccountFacebook() {
-        
-        guard let user = Auth.auth().currentUser else {return}
-        let docRef = FirestoreManager.shared.db.collection("users").document(user.uid)
-        
-        docRef.getDocument { (document, error) in
-    
-            if let document = document, document.exists {
-                //get token from firestore
-                let token = document.get("facebook_access_token") as! String
-                let credential = FacebookAuthProvider.credential(withAccessToken: token)
                 
-                user.reauthenticateAndRetrieveData(with: credential, completion: {(result, error) in
-                    if error != nil {
-                        self.showMessagePrompt("Could not authenticate user.")
-                    } else {
-                        user.delete { error in
-                            if error != nil {
-                                self.showMessagePrompt("Could not delete user.")
-                            } else {
-                                //sign out user after deleting their account
-                                let auth = AuthHandler(auth: Auth.auth())
-                                auth.signOut(onError: self.showMessagePrompt) {
-                                    BuddiesStoryboard.Login.goTo()
-                                }
-                            }
-                        }
-                    }
-                })
-            } else {
-                print("Document does not exist")
+                let _ = storageManger.downloadFile(
+                    for: firebaseImageURL,
+                    to: snapshot.documentID,
+                    session: nil
+                ) { destURL in self.setImage(with: destURL, for: snapshot.documentID) }
             }
+            
+            guard let bio = data?["bio"] as? String else {
+                print("Cannot get bio for \(snapshot.documentID)")
+                return
+            }
+            
+            guard let name = data?["name"] as? String else {
+                print("Cannot get name for \(snapshot.documentID)")
+                return
+            }
+            
+            self.bioLabel.text = bio
+            self.nameLabel.text = name
         }
     }
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setImage(with imageURL: URL, for uid: String){
+        do {
+            let imageData = try Data(contentsOf: imageURL)
+            if let image = UIImage(data: imageData) {
+                onImageLoaded(image: image)
+            } else {
+                print("Failed to load downloaded Topic image for \(uid)")
+            }
+        } catch {
+            print("Could not load topic, \(error)")
+        }
     }
-    */
-
 }
