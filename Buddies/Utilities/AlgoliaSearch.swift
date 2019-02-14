a682655987dd5f8b08162710d7bac59bf59b7d5a
Noah Allen <noahtallen@gmail.com>
2019-02-14 12:50:06 -0500

    Add necessary filter syntax

--- Buddies/Utilities/AlgoliaSearch.swift
+++ Buddies/Utilities/AlgoliaSearch.swift
@@ -10,21 +10,54 @@ import Foundation
 import InstantSearchClient
 
 class AlgoliaSearch {
-    
     // Searches for Activities with optional params
     static func searchActivities(text: String? = nil,
                           topicIds: [String]? = nil,
                           startDate: Date,
                           endDate: Date,
-                          location: (Double, Double)) {
+                          locationRadius: UInt = 20000, // In meters, defaults to 20km
+                          location: (Double, Double)) { // Tuple: (lat, lng)
         
         let client = Client(appID: "YourApplicationID", apiKey: "YourAPIKey")
         let index = client.index(withName: "BUD_ACTIVITIES")
         
-        index.search(Query(query: "jimmie"), completionHandler: { (content, error) -> Void in
+        // Use text search if text is given:
+        let query = text != nil ? Query(query: text) : Query()
+        
+        // We only need the objectID for firebase
+        // query.attributesToRetrieve = ["objectID"]
+        
+        // Location Filter:
+        query.aroundLatLng = LatLng(lat: location.0, lng: location.1)
+        query.aroundRadius = .explicit(locationRadius)
+        
+        // Get filter strings:
+        let dateFilter = getDateFilter(fromDate: startDate, toDate: endDate)
+        let topicFilter = getTopicFilterFrom(topicIds)
+        
+        // Only include topic filter if we want:
+        query.filters = dateFilter + (topicFilter != nil ? "AND \(topicFilter!)" : "")
+        
+        index.search(query, completionHandler: { (content, error) -> Void in
             if error == nil {
                 print("Result: \(content)")
             }
         })
+    }
+    
+    // Note: Any Algolia attribute set up as an array will
+    // match the filter as soon as one of the values in the
+    // array match. (source: https://www.algolia.com/doc/api-reference/api-parameters/filters/#examples)
+    static func getTopicFilterFrom(_ topicIds: [String]?) -> String? {
+        guard let topics = topicIds else { return nil }
+        let withQueryKey = topics.map { "topic_ids:\($0)" }
+        return "(\(withQueryKey.joined(separator: " OR ")))"
+    }
+    
+    // Returns the Algolia Query to match between the two dates.
+    static func getDateFilter(fromDate: Date, toDate: Date) -> String {
+        let startDate = Int(fromDate.timeIntervalSince1970)
+        let endDate = Int(toDate.timeIntervalSince1970)
+        return "(\(startDate) >= start_time_num AND \(endDate) <= end_time_num"
     }
 }
