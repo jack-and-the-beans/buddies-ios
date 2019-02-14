383d0442d1d69a2d597b81b561034662f9f598f4
Jake Thurman <jacob@thurmans.com>
2019-02-13 21:54:36 -0500

    Fix bad text field styling
    We can't get the width of the field from the constructor

--- Buddies/Controls/UnderlinedTextbox.swift
+++ Buddies/Controls/UnderlinedTextbox.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+@IBDesignable
 class UnderlinedTextbox: UITextField, UITextFieldDelegate {
     let border: CALayer
     
@@ -16,37 +17,33 @@ class UnderlinedTextbox: UITextField, UITextFieldDelegate {
         super.init(coder: aDecoder)
         
         delegate = self
-        initBorder()
     }
     required override init(frame: CGRect) {
         border = CALayer()
         super.init(frame: frame)
 
         delegate = self
-        initBorder()
     }
     
-    func initBorder(){
+    override func draw(_ rect: CGRect) {
         self.borderStyle = .none
         
-        let width = CGFloat(2.0)
-        border.borderColor = ControlColors.fieldBorder.cgColor
+        border.backgroundColor = ControlColors.fieldBorder.cgColor
         border.frame = CGRect(
             x: 0,
-            y: self.frame.size.height-width,
-            width: self.frame.size.width + self.frame.size.height,
-            height: self.frame.size.height
+            y: self.bounds.height - 2,
+            width: self.bounds.size.width,
+            height: self.bounds.size.height
         )
-        border.borderWidth = width
         self.layer.addSublayer(border)
         self.layer.masksToBounds = true
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        border.borderColor = ControlColors.fieldBorderFocused.cgColor
+        border.backgroundColor = ControlColors.fieldBorderFocused.cgColor
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        border.borderColor = ControlColors.fieldBorder.cgColor
+        border.backgroundColor = ControlColors.fieldBorder.cgColor
     }
 }
