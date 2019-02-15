4bfb36d508effbd00b82a373871667c9fc0e3d98
Noah Allen <noahtallen@gmail.com>
2019-02-15 13:38:27 -0500

    Add test file for Algolia

---
+++ BuddiesTests/Utilities/AlgoliaTest.swift
@@ -0,0 +1,16 @@
+//
+//  AlgoliaTest.swift
+//  BuddiesTests
+//
+//  Created by Noah Allen on 2/15/19.
+//  Copyright © 2019 Jack and the Beans. All rights reserved.
+//
+
+import Foundation
+
+import XCTest
+@testable import Buddies
+
+class AlgoliaTest: XCTestCase {
+
+}
