5dc6b6241a758abe953c99134e795e5fdd0b9aea
Jake Thurman <jacob@thurmans.com>
2019-02-22 11:34:03 -0500

    Add unit tests for Topic Activity Table view

---
+++ BuddiesTests/Topics/TopicActivityTableVCTests.swift
@@ -0,0 +1,51 @@
+//
+//  TopicActivityTableVCTests.swift
+//  BuddiesTests
+//
+//  Created by Jake Thurman on 2/21/19.
+//  Copyright © 2019 Jack and the Beans. All rights reserved.
+//
+
+import XCTest
+@testable import Buddies
+
+class MockSearchBar : FilterSearchBar {
+    var fetchCount = 0
+    override func fetchAndLoadActivities() {
+        self.fetchCount += 1
+    }
+}
+
+class TopicActivityTableVCTests: XCTestCase {
+    var vc: TopicActivityTableVC!
+    var mockSearchBar: MockSearchBar!
+    
+    override func setUp() {
+        vc = BuddiesStoryboard.Topics.viewController(withID: "topicActivitiesTable")
+        vc.topicId = "j0FFY5VI4Ti6SZ5jUsDJ"
+        
+        UIApplication.setRootView(vc, animated: false)
+        
+        mockSearchBar = MockSearchBar()
+        vc.searchBar = mockSearchBar
+        _ = vc.view // Make sure view is loaded
+    }
+    
+    override func tearDown() {
+        UIApplication.shared.keyWindow?.rootViewController = nil
+        vc = nil
+    }
+    
+    func testInitLifecycle() {
+        XCTAssertNotNil(vc.view, "View should be loaded")
+    }
+    
+    func testHasTopics() {
+        XCTAssert(vc.getTopics().count == 1, "1 topic should be returned!")
+    }
+    
+    func testFetchData() {
+        vc.fetchAndLoadActivities()
+        XCTAssert(mockSearchBar.fetchCount == 1, "expected to fetch once")
+    }
+}
