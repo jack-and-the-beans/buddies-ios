7df26c2fbf2219d4e4d53a1e14e8b458da552f94
Grant Yurisic <grantjyurisic@gmail.com>
2019-03-08 16:54:20 -0500

    Populate My Activities

--- Buddies/MyActivities/MyActivitiesVC.swift
+++ Buddies/MyActivities/MyActivitiesVC.swift
@@ -8,12 +8,61 @@
 
 import UIKit
 import FirebaseAuth
-class MyActivitiesVC: ActivityTableVC, UISearchBarDelegate {
+import Firebase
+class MyActivitiesVC: ActivityTableVC {
 
-    @IBOutlet weak var searchBar: UISearchBar!
-    
     var user: User?
     var cancelUserListener: Canceler?
+    
+    func loadCurUserActivities(userID: String){
+        
+        var created = [ActivityId]()
+        var joined = [ActivityId]()
+        var previous = [ActivityId]()
+        
+        //Get activities current user is associated with
+        FirestoreManager.shared.db.collection("activities").whereField("members", arrayContains: userID).getDocuments(){(querySnapshot, err) in
+            
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    
+                    let endTime  = document.get("end_time") as! Timestamp
+                    
+                    if(endTime.dateValue() < Date())
+                    {
+                        previous.append(document.documentID)
+                    }
+                    else if(document.get("owner_id") as! String == userID)
+                    {
+                        created.append(document.documentID)
+                    }
+                    else
+                    {
+                        joined.append(document.documentID)
+                    }
+                    
+                    
+                }
+            }
+            
+            self.displayIds = [created, joined, previous]
+            
+            self.loadData(for: self.displayIds)
+            
+        }
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let userId = Auth.auth().currentUser?.uid else { return }
+        cancelUserListener = DataAccessor.instance.useUser(id: userId) { user in
+            self.user = user
+        }
+        loadCurUserActivities(userID: userId)
+    }
     
     override func viewDidLoad() {
         
@@ -25,12 +74,26 @@ class MyActivitiesVC: ActivityTableVC, UISearchBarDelegate {
             self.user = user
         }
         
-        searchBar.delegate = self
+        loadCurUserActivities(userID: userId)
         
         super.viewDidLoad()
-
+        
+     
     }
     
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section{
+        case 0:
+            return "Created"
+        case 1:
+            return "Joined"
+        case 2:
+            return "Previous"
+        default:
+            return nil
+        }
+    }
 
     /*
     // MARK: - Navigation
