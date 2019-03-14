c084c1e141ea5e4c21eec9b5dbdfc087d7ad6be7
Grant Yurisic <grantjyurisic@gmail.com>
2019-03-14 15:00:54 -0400

    Add MyActivities screen

--- Buddies/MyActivities/MyActivitiesVC.swift
+++ Buddies/MyActivities/MyActivitiesVC.swift
@@ -7,29 +7,72 @@
 //
 
 import UIKit
-
-class MyActivitiesVC: UITableViewController {
-    var fab: FAB!
+import FirebaseAuth
+import Firebase
+class MyActivitiesVC: ActivityTableVC {
+    
+    var user: LoggedInUser?
+    var cancelUserListener: Canceler?
+    
+    func loadCurUserActivities(userID: String){
+        //Get activities current user is associated with
+        //2D array of activity IDs
+        // [created, joined, previous]
+        FirestoreManager.getUserAssociatedActivities(userID: userID){ result in
+            self.displayIds = result
+            self.loadData(for: self.displayIds)
+        }
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let userId = Auth.auth().currentUser?.uid else { return }
+        cancelUserListener = DataAccessor.instance.useLoggedInUser{ user in
+            self.user = user
+        }
+        loadCurUserActivities(userID: userId)
+    }
     
     override func viewDidLoad() {
+        
+        
+        self.setupHideKeyboardOnTap()
+        
+        guard let userId = Auth.auth().currentUser?.uid else { return }
+        cancelUserListener = DataAccessor.instance.useLoggedInUser{ user in
+            self.user = user
+        }
+        
+        loadCurUserActivities(userID: userId)
+        
         super.viewDidLoad()
-
-        // We need to store a local so that the
-        //  instance isn't deallocated along with
-        //  the event handler!
-        fab = FAB(for: self)
-        fab.renderCreateActivityFab()
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section{
+        case 0:
+            return "Created"
+        case 1:
+            return "Joined"
+        case 2:
+            return "Previous"
+        default:
+            return nil
+        }
     }
-    */
-
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
