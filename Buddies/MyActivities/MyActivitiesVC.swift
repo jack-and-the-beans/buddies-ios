b301357997a2e80fee6777529a6cdbe7d505016f
Luke Meier <lukemeierj@gmail.com>
2019-03-15 19:07:44 -0400

    Filter MyActivities

--- Buddies/MyActivities/MyActivitiesVC.swift
+++ Buddies/MyActivities/MyActivitiesVC.swift
@@ -7,51 +7,71 @@
 //
 
 import UIKit
-import FirebaseAuth
 import Firebase
-class MyActivitiesVC: ActivityTableVC {
+class MyActivitiesVC: ActivityTableVC, UISearchBarDelegate {
     
     var user: LoggedInUser?
     var cancelUserListener: Canceler?
-    
+    var searchTimer: Timer?
+
     @IBOutlet weak var searchBar: UISearchBar!
-    func loadCurUserActivities(userID: String){
-        //Get activities current user is associated with
-        //2D array of activity IDs
-        // [created, joined, previous]
-        FirestoreManager.getUserAssociatedActivities(userID: userID){ result in
-            self.displayIds = result
+    
+    override func fetchAndLoadActivities(for params: SearchParams? = nil) {
+        super.fetchAndLoadActivities(for: params)
+        guard let user = user else { return }
+        
+        FirestoreManager.getUserAssociatedActivities(userID: user.uid){ activities in
+            let filtered = self.filterActivities(activities, query: self.searchBar.text)
+            
+            self.displayIds = filtered.map { $0.map { $0.activityId } }
             self.loadData(for: self.displayIds)
         }
-        
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        guard let userId = Auth.auth().currentUser?.uid else { return }
         cancelUserListener = DataAccessor.instance.useLoggedInUser{ user in
             self.user = user
         }
-        loadCurUserActivities(userID: userId)
+        fetchAndLoadActivities()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        cancelUserListener?()
     }
     
     override func viewDidLoad() {
-        
+        searchBar.delegate = self
         
         self.setupHideKeyboardOnTap()
-        
-        guard let userId = Auth.auth().currentUser?.uid else { return }
         cancelUserListener = DataAccessor.instance.useLoggedInUser{ user in
             self.user = user
         }
-        
-        loadCurUserActivities(userID: userId)
-        
         super.viewDidLoad()
-        
-        
+    }
+
+    func filterActivities(_ activities: [[Activity]], query: String?) -> [[Activity]] {
+        return activities.map { $0.filter { return matchesFilter(activity: $0, query: query) } }
     }
     
+    func matchesFilter(activity: Activity, query: String?) -> Bool {
+        guard let query = query, !query.isEmpty else { return true }
+        
+        let lowerQuery = query.lowercased()
+        return activity.description.lowercased().contains(lowerQuery)
+                || activity.title.lowercased().contains(lowerQuery)
+                || activity.locationText.lowercased().contains(lowerQuery)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // Create a timer to reload stuff so that we don't just reload for every time a letter is pressed in the search bar
+        searchTimer?.invalidate()
+        searchTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { _ in
+            self.fetchAndLoadActivities()
+        }
+    }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch section{
@@ -65,15 +85,6 @@ class MyActivitiesVC: ActivityTableVC {
             return nil
         }
     }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+
     
 }
