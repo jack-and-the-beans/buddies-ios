9566bddf69914df835191efa8e49db49c6a7f008
Jake Thurman <jacob@thurmans.com>
2019-03-24 18:51:06 -0400

    Fix unit tests for DataAccessor invalidation optimization

--- BuddiesTests/Models/ActivityTests.swift
+++ BuddiesTests/Models/ActivityTests.swift
@@ -48,14 +48,14 @@ class ActivityTests: XCTestCase {
         activity.ownerId = "bob_id" // The same on purpose!
         activity.title = "Hello, I'm an event!"
         activity.description = "a new desc"
-        activity.startTime = Timestamp(date: Date().addingTimeInterval(TimeInterval(-50)))
-        activity.endTime = Timestamp(date: Date().addingTimeInterval(TimeInterval(-10)))
+        activity.startTime = Timestamp(date: Date().addingTimeInterval(TimeInterval(50)))
+        activity.endTime = Timestamp(date: Date().addingTimeInterval(TimeInterval(10)))
         
-        activity.topicIds = []
-        activity.topicIds = ["x", "y"] // Indecisive!
+        activity.topicIds = ["x", "y"]
+        activity.topicIds = ["x", "y"] // The same on purpose!
         
         XCTAssert(deli.invalidations == deli.triggers, "should call both")
-        XCTAssert(deli.invalidations == 9, "one per line here. no de-dup expected")
+        XCTAssert(deli.invalidations == 7, "Called once per CHANGE (not repeats)")
     }
     
     func testInit(){
