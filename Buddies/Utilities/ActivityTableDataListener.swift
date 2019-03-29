6ddefd6318f330b4323c0c0cbc7fe3850dff6f3c
Noah Allen <noahtallen@gmail.com>
2019-03-28 21:28:55 -0400

    Implement useActivities in table data handler

--- Buddies/Utilities/ActivityTableDataListener.swift
+++ Buddies/Utilities/ActivityTableDataListener.swift
@@ -10,35 +10,76 @@
 // in order to handle live updates from the data
 // source.
 protocol ActivityTableDataDelegate {
-    func onActivityUpdate(updatedActivities: [[Activity]])
+    func onActivityUpdate(updatedActivity: Activity)
     func onNewActivities(newActivities: [[Activity]])
-    func onRemoveActivities(activityIds: [[ActivityId]])
+    func onRemoveActivity(activityId: ActivityId)
     func onOperationsFinished()
 }
 
 class ActivityTableDataListener {
-    var wantedActivities = [[ActivityId]]()
+    let dataAccessor = DataAccessor.instance
     var delegate: ActivityTableDataDelegate? = nil
 
+    var cancelers = [Canceler]()
+
+    // A list of the activities we want to get:
+    var wantedActivities = [[ActivityId]]()
+    // A list of the activities for which we've
+    // handled the initial setup. The boolean
+    // denotes whether or not the activity DNE.
+    var handledActivities = [ActivityId : Bool]()
+
     // Returns the nested array flattened to a single list, in order.
-    fileprivate func getStringsInOrder(_ ids: [[String]]) {
+    fileprivate func getStringsInOrder(_ ids: [[String]]) -> [String] {
         var allids = ids.flatMap { $0 }
-        return allids.sort()
+        allids.sort()
+        return allids
     }
     
     func updateWantedActivities(with ids: [[ActivityId]]) {
+        let orderedNewIds = getStringsInOrder(ids)
         // Checks to see if the arrays are different. If they are the same,
         // cancel the operation. Otherwise, continue getting the activities.
-        guard (getStringsInOrder(ids) != getStringsInOrder(wantedActivities)) else {
+        if (orderedNewIds == getStringsInOrder(wantedActivities)) {
             delegate?.onOperationsFinished()
             return
         }
+
         self.wantedActivities = ids
-        // @TODO: Ask firebase for dem activities.
+        self.handledActivities = [:]
+        self.cleanup()
+
+        let newActivities = [[Activity]]()
+        
+        self.cancelers = ids.enumerated().flatMap { i, idList in
+            return idList.enumerated().map { j, id in
+                return dataAccessor.useActivity(id: id) { activity in
+                    if (self.handledActivities[id] == nil && activity != nil) {
+                        // DNE yet but we have the activity
+                        // @TODO: save the activity
+                        self.handledActivities[id] = true
+                        if (self.handledActivities.count == orderedNewIds.count) {
+                            // Now, all activities have been handled
+                            self.delegate?.onNewActivities(newActivities: newActivities)
+                        }
+                    } else if self.handledActivities[id] == nil {
+                        // DNE yet, but the activity is nil
+                        self.handledActivities[id] = false
+                    } else if let activity = activity {
+                        // Already exists, and we have an activity
+                        self.delegate?.onActivityUpdate(updatedActivity: activity)
+                        self.handledActivities[id] = true
+                    } else {
+                        // Already exists, but the activity is now nil
+                        self.delegate?.onRemoveActivity(activityId: id)
+                        self.handledActivities[id] = false
+                    }
+                }
+            }
+        }
     }
     
     func cleanup() {
-        // @TODO: Implement cleanup
+        self.cancelers.forEach { $0() }
     }
-    
 }
