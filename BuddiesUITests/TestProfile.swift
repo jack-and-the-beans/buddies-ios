4f64987c07be91568f7425e0402fe9649e7c4254
Luke Meier <lukemeierj@gmail.com>
2019-04-03 10:43:36 -0400

    Test running through Profile

--- BuddiesUITests/TestProfile.swift
+++ BuddiesUITests/TestProfile.swift
@@ -8,27 +8,32 @@
 
 import XCTest
 
-class TestProfile: XCTestCase {
+class TestProfile: BuddiesUITest {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testEditProfile() {
+        login()
+        let profile = app.tabBars.buttons["Profile"]
+        
+        XCTAssertTrue(waitForElementToAppear(profile, timeout: 15), "Profile tab button appears")
+        print(app.debugDescription)
+        profile.tap()
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let profileNavigationBar = app.navigationBars["Profile"]
+        let settingsBtn = profileNavigationBar.buttons["Settings"]
+        
+        XCTAssertTrue(waitForElementToAppear(settingsBtn, timeout: 5), "Settings button appears on Profile")
+        
+        settingsBtn.tap()
+        
+        app.navigationBars["Settings"].buttons["Profile"].tap()
+        
+        let editButton = profileNavigationBar.buttons["Edit"]
+        editButton.tap()
+        app.buttons["Cancel"].tap()
+        editButton.tap()
+        app.buttons["Save"].tap()
+        
     }
 
 }
