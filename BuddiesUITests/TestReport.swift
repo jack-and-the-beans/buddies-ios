d7a334b65e82f5abcb597ed5d2fde0dae147813b
Luke Meier <lukemeierj@gmail.com>
2019-04-02 21:06:31 -0400

    Remove UITest for now

--- BuddiesUITests/TestReport.swift
+++ BuddiesUITests/TestReport.swift
@@ -9,18 +9,18 @@
 import XCTest
 
 class TestReport: BuddiesUITest {
-    func testReportButton() {
-        self.login()
-        sleep(2)
-        app.tables.cells.allElementsBoundByIndex.first?.tap()
-        app.navigationBars["View Activity"]/*@START_MENU_TOKEN@*/.buttons["reportActivity"]/*[[".buttons[\"Report\"]",".buttons[\"reportActivity\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        
-        let reportActivityNavigationBar = app.navigationBars["Report Activity"]
-        let sendButton = reportActivityNavigationBar.buttons["Send"]
-        //It gets really angry when I try to use the "Cancel" button
-        sendButton.tap()
-        XCTAssertTrue(sendButton.isHittable, "Nothing typed, so Send does nothing")
-
-    }
+//    func testReportButton() {
+//        self.login()
+//        sleep(10 )
+//        app.tables.cells.allElementsBoundByIndex.first?.tap()
+//        app.navigationBars["View Activity"]/*@START_MENU_TOKEN@*/.buttons["reportActivity"]/*[[".buttons[\"Report\"]",".buttons[\"reportActivity\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+//        
+//        let reportActivityNavigationBar = app.navigationBars["Report Activity"]
+//        let sendButton = reportActivityNavigationBar.buttons["Send"]
+//        //It gets really angry when I try to use the "Cancel" button
+//        sendButton.tap()
+//        XCTAssertTrue(sendButton.isHittable, "Nothing typed, so Send does nothing")
+//
+//    }
 
 }
